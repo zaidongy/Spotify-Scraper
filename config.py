8035494dcb2535006eec915c39e88cd3a9ce6347
Chris Yang <42453260+zaidongy@users.noreply.github.com>
2024-02-18 22:09:31 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+DATABASE_CONFIG = {
+    'host': 'mongodb://{0}:{1}@MONGODB_HOST/DB_COLLECTION_NAME',
+    'dbuser': '<database_user>', # Database user
+    'dbuserpassword': '<database_password>', # Database password
+}
